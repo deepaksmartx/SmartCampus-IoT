3532669b026dcfeb9cf73bcd6821afd1404fca85
Shivam kundu <kundushivam38@gmail.com>
2026-03-27 14:19:16 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = postgresql://neondb_owner:npg_0OdUvLMPKRJ8@ep-polished-thunder-a1wx5sks-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require
 
 print("DEBUG DB URL:", DATABASE_URL)
 
