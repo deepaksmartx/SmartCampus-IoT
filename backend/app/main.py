c47c40e79b24a3e604afd3e0167a4b191da70949
Shivam kundu <kundushivam38@gmail.com>
2026-04-10 12:05:47 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routes import auth, facility, booking
+from app.routes import iot
+
+app.include_router(iot.router)
 
 Base.metadata.create_all(bind=engine)
 
