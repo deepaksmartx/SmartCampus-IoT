98e63751b3e0b73aa7f505acc36e5427ba3cc6db
Shivam kundu <kundushivam38@gmail.com>
2026-04-15 13:58:23 +0530

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,7 +5,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    role = Column(String)  # student / faculty / admin
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, default="student")  # student / faculty / admin / manager
