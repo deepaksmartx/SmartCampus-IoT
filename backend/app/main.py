2274d14496a8a5e53e8162bd1d6b9d33f5d94f88
Poorvika Bhatia <purvibhatia3732@gmail.com>
2026-04-29 20:52:04 +0530

    fixed deployment issues

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,12 +12,5 @@ Base.metadata.create_all(bind=engine)
 
 
 #Step 3: Include routers
-app.include_router(routes.auth.router)
-app.include_router(routes.facility.router)
-app.include_router(routes.booking.router)
-
-app.include_router(routes.iot.router)
-
-app.include_router(routes.iot.router)
-app.include_router(routes.manager.router)
+app.include_router(routes.router)
 
